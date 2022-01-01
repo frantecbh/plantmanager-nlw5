d978da4a85f8ad6cd2daa98229cf530f0e9f8e3c
Francisco Menezes <f07785c@fiatauto.adfa.local>
2022-01-01 12:12:00 -0300

    first commit

--- App copy.tsx
+++ App copy.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>NLW</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
